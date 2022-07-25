35a16f2ff16a558296de7604cf9d12c566ec08ed
mubu <mubu002@EGMS.no>
2022-07-25 22:46:39 +0200

    Navigation with react router

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, Link, Route, Routes} from "react-router-dom";
 
 
 function FontPage() {
@@ -13,18 +13,24 @@ function FontPage() {
     </div>;
 }
 
-function MovieApplication() {
+function MovieApplication(){
+    return <Routes>
+        <Route path={"/"} element={<FontPage />} />
+        <Route path={"/new"} element={<h1>Create new movie</h1>}/>
+    </Routes>
+}
+
+function Application() {
     return <BrowserRouter>
         <Routes>
             <Route path={"/"} element={<FontPage />} />
-            <Route path={"/movies"} element={<h1>Movies</h1>}/>
-            <Route path={"/movies/new"} element={<h1>Create new movie</h1>}/>
+            <Route path={"/movies/*"} element={<MovieApplication />}/>
             <Route path={"*"} element={<h1>Not Found</h1>} />
         </Routes>
     </BrowserRouter>;
 }
 
 ReactDOM.render(
-    <MovieApplication/>,
+    <Application/>,
     document.getElementById("app")
 );
